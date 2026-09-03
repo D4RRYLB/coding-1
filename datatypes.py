ca8fab2a2f44ecd78194f767050a23012592c135
D4RRYLB <dbrown386@boyslatin.net>
2026-09-03 13:44:10 +0000

    update 9/3

---
+++ datatypes.py
@@ -0,0 +1,17 @@
+    #String- characters that are wrapped within quotes.
+    #characters can be letters, numbers, and symbols.
+    #you can use both single and double quotation
+    #marks for strings
+"Darryl Brown"
+123 main street
+this cost 100 dollars
+#the hashtag is a marker that makes anything written
+#behind it invisible to the computer
+Interger- datatypes that are just whole numbers, or numbers
+without a decimal print
+we dont have to add any special symbols, we can write the number
+as it
+# FLOATS- A number with a decimal.
+ BOOLEAN- a data type that can only be either true or false
+# we do not use quotation marks for boolean values. the computer 
+# understand these words as something unique.
---
+++ variableActivity.py
@@ -0,0 +1,36 @@
+variable- A conitainer that stores data. you can
+#name your conitainer almost anything you want, so long
+#as it is decriptive
+
+Name- Darryl Brown
+yearsofexp- 10
+graduate- true
+
+#when we want to create a variable,we make up a word,
+#use the equal sign and then assign it a data type
+#whatever the computer sees our variable word it will
+#always use the data inside
+
+#variable Naming rules
+#A variable MUST start with letter and cannot start with a numeber
+#a variable can be a mixed of letters and numbers but not symbols
+#a variable cannot be a python keyword
+
+
+Numberofapple= 50
+Numberofstudentinclass= 21
+thecostoftravelinsurance= 4,500
+
+priceofpepsi= 3.50
+priceofcoke= 3.50
+priceoffanta= 3
+
+#camal case- when creating variable names with multiple words,
+#every new word begins with a capital letter
+#the reason why its called camal is because every new word
+#represents a hump
+
+#Snake case- when creating a variable using snake case
+#we use a undercore symbols to space out each new word
+#the reason why its called snake case is because the undercore
+#represents a snake on the ground new word
