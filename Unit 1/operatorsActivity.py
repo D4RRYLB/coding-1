5a4583337a685fdbe382c093d66b177f7ecb2a0e
D4RRYLB <dbrown386@boyslatin.net>
2026-09-16 13:41:41 +0000

    change to Operator activity

--- Unit 1/operatorsActivity.py
+++ Unit 1/operatorsActivity.py
@@ -9,3 +9,24 @@ print ("coding 1" == "coding 1")
 
 print (0 != 0)
 #false
+
+Book = 10.99
+tablet = 399.99
+
+print(book + tablet)
+
+Cart = 98.87
+amountTogetDiscount = 100.00
+
+print(carts < amountTogetDiscount)
+
+shoes = 200.00
+Tax = 0.07
+
+print(shoes * Tax)
+
+GPA = 86
+Recommendation = true
+
+print(GPA > 85 and Recommendation == True)
+
